ec3dcf62077944fa0487aaed720aa818f5e34fc8
Ebraim Oliveira <ebraim_oliveira@hotmail.com>
2023-05-29 16:30:08 -0300

    funcao verifica palindromo aplicada

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,57 @@
-def is_anagram(first_string, second_string):
-    
-    raise NotImplementedError
+# def bubble_sort(string):
+#     string = list(string.strip().lower().replace(" ", ""))
+#     for iteracao in range(len(string)-1, 0, -1):
+#         for i in range(iteracao):
+#             if string[i] > string[i + 1]:
+#                 temp = string[i]
+#                 string[i] = string[i + 1]
+#                 string[i + 1] = temp
+#     return (string)
+
+def merge_sort(vector):
+    length = len(vector)
+    if length > 1:
+        mid = length // 2
+        left_vector = vector[:mid]
+        right_vector = vector[mid:]
+
+        # recursion
+        merge_sort(left_vector)
+        merge_sort(right_vector)
+
+        merge(vector, left_vector, right_vector)
+
+    return
+
+
+def merge(vector, left_vector, right_vector):
+    # left_vector idx, right_vector idx, merged vector idx
+    i = j = k = 0
+    while i < len(left_vector) and j < len(right_vector):
+        if left_vector[i] <= right_vector[j]:
+            vector[k] = left_vector[i]
+            i += 1
+        else:
+            vector[k] = right_vector[j]
+            j += 1
+        k += 1
+
+    while i < len(left_vector):
+        vector[k] = left_vector[i]
+        i += 1
+        k += 1
+
+    while j < len(right_vector):
+        vector[k] = right_vector[j]
+        j += 1
+        k += 1
+
+
+def is_anagram(fst_string, scnd_string):
+    merge_sort(list(fst_string.strip().lower().replace(' ', '')))
+    merge_sort(list(scnd_string.strip().lower().replace(' ', '')))
+    true_or_false = True
+    if fst_string != scnd_string or len(fst_string) == 0 == len(scnd_string):
+        true_or_false = False
+
+    return (''.join(fst_string), ''.join(scnd_string), true_or_false)
--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,16 @@
-def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def is_palindrome_recursive(word, low, high):
+    word = word.strip().lower().replace(' ', '')
+
+    if len(word) == 0:
+        return False
+
+    if word[low] == word[high]:
+        low += 1
+        high -= 1
+
+        if low >= high:
+            return True
+
+        return is_palindrome_recursive(word, low, high)
+
+    return False
