735df2f050941f2f23063f0bd8ce7c69e994855b
Ebraim Oliveira <ebraim_oliveira@hotmail.com>
2023-05-30 18:01:06 -0300

    study_schedule in progress

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -4,7 +4,6 @@ time = []
 
 
 def periods(tupla):
-    
     if type(tupla[0]) != 'int' or type(tupla[1]) != 'int':
         return None
     for i in range(tupla[0], tupla[1] + 1):
@@ -16,13 +15,9 @@ def study_schedule(permanence_period, target_time):
         return None
 
     for i in permanence_period:
-        if periods(i) == None:
-            return None
         periods(i)
 
     most_common = Counter(time).most_common(1)
     print('AQUIII', most_common)
 
     return most_common
-
-# study_schedule([(1,2),(3,8),(1,3),(2,2)],0)
