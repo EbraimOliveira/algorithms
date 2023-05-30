287e265fd4f9921ec0affd93214b263203521eac
Ebraim Oliveira <ebraim_oliveira@hotmail.com>
2023-05-30 17:54:50 -0300

    study_schedule in progress

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,28 @@
+from collections import Counter
+
+time = []
+
+
+def periods(tupla):
+    
+    if type(tupla[0]) != 'int' or type(tupla[1]) != 'int':
+        return None
+    for i in range(tupla[0], tupla[1] + 1):
+        time.append(i)
+
+
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if target_time is None:
+        return None
+
+    for i in permanence_period:
+        if periods(i) == None:
+            return None
+        periods(i)
+
+    most_common = Counter(time).most_common(1)
+    print('AQUIII', most_common)
+
+    return most_common
+
+# study_schedule([(1,2),(3,8),(1,3),(2,2)],0)
