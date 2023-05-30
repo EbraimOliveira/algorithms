7b8e7d0d2248d461e5f17b2edd58a0c27aa0ea5e
Ebraim Oliveira <ebraim_oliveira@hotmail.com>
2023-05-30 14:50:17 -0300

    teste aplicado

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,12 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError, match='tipo inválido para key'):
+        encrypt_message('string', 'string')
+    with pytest.raises(TypeError, match='tipo inválido para message'):
+        encrypt_message(666, 666)
+    assert encrypt_message('arara', 2) == 'ara_ra'
+    assert encrypt_message('arara', 3) == 'ara_ar'
+    assert encrypt_message('string', 999) == 'gnirts'
